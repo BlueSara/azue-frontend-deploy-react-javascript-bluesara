f5db5c70a3a9e0f617f37670ac203f838da66076
Sara Boudzakhet <152734791+BlueSara@users.noreply.github.com>
2026-09-23 11:25:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'Everyone';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}</div> <div><h1> HI </h1> </div>;
 }
 
 export default App;
